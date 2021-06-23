040b40d85392e3e89e360362feec9c9b0952a0d0
gibdike <vitoria.angel2002@gmail.com>
2021-06-23 19:51:05 -0400

    dia 3

---
+++ lib/modules/home/home_page.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(152),
+        child: Container(
+          height: 152,
+          color: AppColors.primary,
+          child: Center(
+            child: ListTile(
+              title: Text("Ola, Angel"),
+              subtitle: Text("Mantenha suas contas em dia"),
+              trailing: Container(
+                height: 48,
+                width: 48,
+                decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(5)),
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
